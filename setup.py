7d40b23622c19f8eea935b72ad8f16c7d47fa45d
coolspy007 <68081755+coolspy007@users.noreply.github.com>
2024-03-17 21:10:54 -0400

    Requirements added

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Chicken-disease-classification-project"
+AUTHOR_USER_NAME = "coolspy007"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "krishnakumar.balu@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    package_dir = {"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
